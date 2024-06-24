0aaaf9a69fa187f5603cb3da44e958d8944f239f
jstalinko <alinkodotid@gmail.com>
2024-06-24 22:50:46 +0700

    first commit

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,18 +1,17 @@
 <template>
   <ion-page>
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
-
-      <ExploreContainer name="Tab 1 page" />
+   
+    <ion-content>
+      <ion-grid>
+        <ion-row>
+          <ion-col>
+            My Bottles
+          </ion-col>
+          <ion-col>
+            Points
+          </ion-col>
+        </ion-row>
+      </ion-grid>
     </ion-content>
   </ion-page>
 </template>
