9332e0229fabcd67e7e47d1059ca1b1f921a9964
jstalinko <alinkodotid@gmail.com>
2024-06-25 22:26:58 +0700

    dashboard

--- src/views/home/Index.vue
+++ src/views/home/Index.vue
@@ -1,10 +1,50 @@
 <template>
   <ion-page>
     <ion-content>
-      <ion-grid :fixed="true">
+      <ion-img src="../../../resources/icon.png" style="width:120px;height: 150px;margin:0 auto;" />
+      <h4 class="ion-text-center">Welcome , Sonata !</h4>
+      <ion-grid :fixed="true" class="goza-area">
         <ion-row>
-          <ion-col> My Bottles </ion-col>
-          <ion-col> Points </ion-col>
+          <ion-col class="ion-text-center br-1">
+            <ion-icon :icon="beerOutline"></ion-icon>
+            <ion-text>My Bottles</ion-text> <br>
+            <b>10</b>
+          </ion-col>
+          <ion-col class="ion-text-center bl-1">
+            <ion-icon :icon="cardOutline"></ion-icon>
+            <ion-text>My Points</ion-text>
+            <br>
+            <b>10</b>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-grid class="flex flex-col flex-wrap align-items-center">
+        <ion-row>
+          <ion-col>
+            <ion-button color="primary" class="text-reset">
+              <ion-icon :icon="beerOutline" size="large"></ion-icon>
+              <ion-text>My Bottles</ion-text>
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button color="primary" class="text-reset">
+              <ion-icon :icon="ticketOutline" size="large"></ion-icon>
+              My Tickets
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button color="primary" class="text-reset">
+              <ion-icon :icon="mailOpenOutline" size="large"></ion-icon>
+              RSVP
+            </ion-button>
+          </ion-col>
+          <ion-col>
+            <ion-button color="primary" class="text-reset">
+              <ion-icon :icon="mapOutline" size="large"></ion-icon>
+              Outlet
+            </ion-button>
+          </ion-col>
         </ion-row>
       </ion-grid>
     </ion-content>
@@ -12,14 +52,31 @@
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonContent } from "@ionic/vue";
+import {
+  IonPage,
+  IonContent,
+  IonIcon,
+  IonText,
+  IonButton
+} from "@ionic/vue";
+import { beerOutline, cardOutline, mailOpenOutline, mapOutline, ticketOutline, wineOutline } from "ionicons/icons";
 </script>
 
 <style scoped>
-ion-col {
-  background-color: #135d54;
-  border: solid 1px #fff;
+.goza-area {
+  border-radius: 100px !important;
+  background-color: var(--ion-color-primary);
+  padding: 13px;
   color: #fff;
-  text-align: center;
+  width: 90%;
+  max-width: 96%;
+}
+
+.br-1 {
+  border-right: 1px solid #ddd;
+}
+
+.bl-1 {
+  border-left: 1px solid #ddd;
 }
 </style>
