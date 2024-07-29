db8ecf33354e3d79811569615d1f48e09b81f8f5
reiyanyan <hariyantoriyan.hr@gmail.com>
2024-07-29 14:13:16 +0700

    fix: request salinko

--- src/views/home/Offers.vue
+++ src/views/home/Offers.vue
@@ -1,99 +1,186 @@
 <template>
-    <ion-page>
-        <ion-content>
-            <ion-header collapse="condense" class="mt-header">
-                <ion-toolbar>
-                    <ion-title>
-                        <span v-if="category=='event'">
-                            {{ category.toUpperCase() }}
-                        </span>
-                        <span v-else>
-                            PROMO {{ category.toUpperCase() }}
-                        </span>
-                    </ion-title>
-                </ion-toolbar>
-            </ion-header>
+  <ion-page>
+    <ion-content>
+      <ion-header collapse="condense" class="mt-header">
+        <ion-toolbar>
+          <ion-title>
+            <span v-if="category == 'event'">
+              {{ category.toUpperCase() }}
+            </span>
+            <span v-else> PROMO {{ category.toUpperCase() }} </span>
+          </ion-title>
+        </ion-toolbar>
+      </ion-header>
 
-            <div class="ion-padding" style="margin-top: 20px">
-                <ion-segment value="buttons" v-model="activeTab">
-                    <ion-segment-button value="upcoming">
-                        <ion-label>UP-COMING</ion-label>
-                    </ion-segment-button>
-                    <ion-segment-button value="ongoing">
-                        <ion-label>ON-GOING</ion-label>
-                    </ion-segment-button>
-                    <ion-segment-button value="finished">
-                        <ion-label>FINISHED</ion-label>
-                    </ion-segment-button>
-                </ion-segment>
-               
-                <br />
+      <div class="ion-padding" style="margin-top: 20px">
+        <ion-segment value="buttons" v-model="activeTab">
+          <ion-segment-button value="upcoming">
+            <ion-label>UP-COMING</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="ongoing">
+            <ion-label>ON-GOING</ion-label>
+          </ion-segment-button>
+          <ion-segment-button value="finished">
+            <ion-label>FINISHED</ion-label>
+          </ion-segment-button>
+        </ion-segment>
 
-                <NoData :message="'while no data is displayed'" v-if="eventsData.length < 1"/>
+        <br />
 
-                <ion-card v-for="(event, index) in eventsData" :key="index" v-if="eventsData.length > 0">
-                    <ion-img :alt="event.name" :src="imageUrl(event.image)" />
-                    <ion-card-header>
-                        <ion-card-title>{{ event.name }}</ion-card-title>
-                        <ion-card-subtitle class="flex justify-between " color="primary">{{ new Date(event.start_date).toLocaleDateString() }} - {{ new
-                            Date(event.end_date).toLocaleDateString() }} 
-                            <ion-badge>{{ event.type }}</ion-badge>
-                            <ion-badge :color="badgeColor[event.status]">{{ event.status }}</ion-badge>    
-                        </ion-card-subtitle>
-                    </ion-card-header>
+        <NoData
+          :message="'while no data is displayed'"
+          v-if="eventsData.length < 1"
+        />
 
-                    <ion-card-content>
-                        {{ event.description }}
-                        <ion-button expand="block" :color="badgeColor[event.status]">
-                            <span v-if="event.status == 'upcoming' || event.status == 'ongoing'">
-                                <ion-icon :icon="calendarClearOutline"></ion-icon> Reservasi
-                            </span>
-                            <span v-else>
-                                <ion-icon :icon="imageOutline"></ion-icon> Gallery
-                            </span>
-                        </ion-button>
-                    </ion-card-content>
-                </ion-card>
-            </div>
-        </ion-content>
-    </ion-page>
+        <ion-card
+          v-for="(event, index) in eventsData"
+          :key="index"
+          v-if="eventsData.length > 0"
+        >
+          <ion-img :alt="event.name" :src="imageUrl(event.image)" />
+          <ion-card-header>
+            <ion-card-title>{{ event.name }}</ion-card-title>
+
+            <ion-card-subtitle color="primary">
+              <ion-grid>
+                <ion-row>
+                  <ion-col>
+                    {{ dateFormatter(event.start_date) }} -
+                    {{ dateFormatter(event.end_date) }}
+                  </ion-col>
+                  <ion-col>
+                    <ion-grid>
+                      <ion-row class="bad">
+                        <ion-col
+                          size="6"
+                          size-sm="6"
+                          size-md="6"
+                          size-lg="6"
+                          size-xl="6"
+                        >
+                          <ion-badge>{{ event.type }}</ion-badge>
+                        </ion-col>
+                        <ion-col
+                          size="6"
+                          size-sm="6"
+                          size-md="6"
+                          size-lg="6"
+                          size-xl="6"
+                        >
+                          <ion-badge :color="badgeColor[event.status]">
+                            {{ event.status }}
+                          </ion-badge>
+                        </ion-col>
+                      </ion-row>
+                    </ion-grid>
+                  </ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-subtitle>
+          </ion-card-header>
+
+          <ion-card-content>
+            {{ event.description }}
+            <ion-button expand="block" :color="badgeColor[event.status]">
+              <span
+                v-if="event.status == 'upcoming' || event.status == 'ongoing'"
+              >
+                <ion-icon :icon="calendarClearOutline"></ion-icon> Reservasi
+              </span>
+              <span v-else>
+                <ion-icon :icon="imageOutline"></ion-icon> Gallery
+              </span>
+            </ion-button>
+          </ion-card-content>
+        </ion-card>
+      </div>
+    </ion-content>
+  </ion-page>
 </template>
 
 <script setup lang="ts">
-import NoData from '@/components/NoData.vue';
-import { getEvent } from '@/composables/Http';
-import { imageUrl, Loading } from '@/composables/Utils';
-import { HttpResponse } from '@capacitor/core';
-import { IonPage, IonContent, IonHeader, IonToolbar, IonSegment, IonSegmentButton, IonLabel, IonTitle, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonImg,IonBadge,IonButton,IonIcon } from '@ionic/vue';
-import { calendarClearOutline, imageOutline } from 'ionicons/icons';
-import { onMounted, ref, watch } from 'vue';
-import { useRoute } from 'vue-router';
-
+import NoData from "@/components/NoData.vue";
+import { getEvent } from "@/composables/Http";
+import { imageUrl, Loading } from "@/composables/Utils";
+import { HttpResponse } from "@capacitor/core";
+import {
+  IonPage,
+  IonContent,
+  IonHeader,
+  IonToolbar,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonTitle,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonImg,
+  IonBadge,
+  IonButton,
+  IonIcon,
+  IonRow,
+  IonGrid,
+  IonCol,
+} from "@ionic/vue";
+import { calendarClearOutline, imageOutline } from "ionicons/icons";
+import { onMounted, ref, watch } from "vue";
+import { useRoute } from "vue-router";
 
 const route = useRoute();
-const category:any = ref(route.params.category);
-const activeTab = ref('ongoing');
-const eventsData:any = ref([]);
+const category: any = ref(route.params.category);
+const activeTab = ref("ongoing");
+const eventsData: any = ref([]);
 const badgeColor: any = ref({
-    finished: 'danger',
-    ongoing: 'success',
-    upcoming: 'primary'
+  finished: "danger",
+  ongoing: "success",
+  upcoming: "primary",
 });
 
-watch(activeTab, async() => await getEvents());
+watch(activeTab, async () => await getEvents());
 const getEvents = async () => {
-    await Loading(1000,"Please wait...");
-    let cat: any = (category.value == 'event') ? null : category.value;
-    let resp: HttpResponse = await getEvent(cat);
-    if (cat == null) {
-        eventsData.value = resp.data.data.filter((ev : any) => ev.status == activeTab.value);
-    } else {
-        eventsData.value = resp.data.data.filter((ev:any) => ev.type == category.value).filter((s:any) => s.status == activeTab.value);
-    }
+  await Loading(1000, "Please wait...");
+  const cat: any = category.value == "event" ? null : category.value;
+  const resp: HttpResponse = await getEvent(cat);
+  if (cat == null) {
+    eventsData.value = resp.data.data.filter(
+      (ev: any) => ev.status == activeTab.value
+    );
+  } else {
+    eventsData.value = resp.data.data
+      .filter((ev: any) => ev.type == category.value)
+      .filter((s: any) => s.status == activeTab.value);
+  }
+};
+
+function dateFormatter(date: string) {
+  return new Intl.DateTimeFormat("id-ID", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }).format(new Date(date));
 }
 
 onMounted(async () => {
-    await getEvents();
+  await getEvents();
 });
-
 </script>
+
+<style lang="scss" scoped>
+ion-card-title {
+  &.ios,
+  &.md {
+    font-size: 1.5rem;
+  }
+}
+
+ion-row {
+  &.bad {
+    flex-wrap: nowrap;
+    flex-direction: row;
+    // gap: 2px;
+  }
+}
+</style>
--- src/views/home/Rsvp.vue
+++ src/views/home/Rsvp.vue
@@ -14,45 +14,51 @@
       </ion-card>
 
       <ion-card>
-        <img :alt="outlet_details.name" :src="imageUrl(outlet_details.area_image)" />
+        <img
+          :alt="outlet_details.name"
+          :src="imageUrl(outlet_details.area_image)"
+        />
 
         <ion-card-header>
           <ion-card-subtitle>Reservation Form</ion-card-subtitle>
         </ion-card-header>
         <ion-card-content>
-          <b>
-            Choose Date
-          </b>
-          <ion-datetime-button datetime="datetime" ></ion-datetime-button>
-          <ion-modal :keep-contents-mounted="true" >
+          <b> Choose Date </b>
+          <ion-datetime-button datetime="datetime"></ion-datetime-button>
+          <ion-modal :keep-contents-mounted="true">
             <ion-datetime id="datetime" v-model="dateSelected"></ion-datetime>
           </ion-modal>
-          <br>
-          <b>
-            Choose Floor
-          </b>
+          <br />
+          <b> Choose Floor </b>
           <ion-segment v-model="floorSelected">
-            <ion-segment-button v-for="(floor, index) in floors" :value="floor.floor" :key="index">
-              <ion-label color="primary">{{ getOrdinalSuffix(floor.floor) }}</ion-label>
+            <ion-segment-button
+              v-for="(floor, index) in floors"
+              :value="floor.floor"
+              :key="index"
+            >
+              <ion-label color="primary">{{
+                getOrdinalSuffix(floor.floor)
+              }}</ion-label>
             </ion-segment-button>
           </ion-segment>
 
-          <br>
-          <b>
-            Choose Table(s)
-          </b>
+          <br />
+          <b> Choose Table(s) </b>
           <ion-buttons slot="start">
-            <ion-button v-for="(tbl, index) in tables" :key="index" @click="selectTable(tbl, tbl.id, floorSelected)"
-              :fill="carts.find((a: any) => a?.id == tbl.id) ? 'solid' : 'outline'">{{ tbl.code }}</ion-button>
+            <ion-button
+              v-for="(tbl, index) in tables"
+              :key="index"
+              @click="selectTable(tbl, tbl.id, floorSelected)"
+              :fill="carts.find((a: any) => a?.id == tbl.id) ? 'solid' : 'outline'"
+              >{{ tbl.code }}</ion-button
+            >
           </ion-buttons>
         </ion-card-content>
       </ion-card>
 
       <ion-card v-if="carts.length > 0">
         <ion-card-header>
-          <ion-card-subtitle>
-            Summary
-          </ion-card-subtitle>
+          <ion-card-subtitle> Summary </ion-card-subtitle>
         </ion-card-header>
 
         <ion-card-content>
@@ -60,57 +66,107 @@
             <ion-item>Reservation for : {{ dateSelected }}</ion-item>
             <ion-item v-for="(cart, index) in carts" :key="index">
               <ion-label>
-                {{ getOrdinalSuffix(cart.floor) }} Floor - {{ cart.table }} - {{ cart.max_pax }} Max Pax - {{
-                  currencyIDR(cart.price) }}
+                {{ getOrdinalSuffix(cart.floor) }} Floor - {{ cart.table }} -
+                {{ cart.max_pax }} Max Pax - {{ currencyIDR(cart.price) }}
               </ion-label>
             </ion-item>
           </ion-list>
-          <br>
+          <br />
 
           <ion-item>
-            <ion-select label="Payment Method" label-placement="floating" v-model="payment_method">
-              <ion-select-option value="transfer">BANK TRANSFER</ion-select-option>
+            <ion-select
+              label="Payment Method"
+              label-placement="floating"
+              v-model="payment_method"
+            >
+              <ion-select-option value="transfer"
+                >BANK TRANSFER</ion-select-option
+              >
               <ion-select-option value="qris">QRIS</ion-select-option>
             </ion-select>
           </ion-item>
 
-          <br>
-          <ion-button expand="full"><ion-icon :icon="sendOutline"></ion-icon>&nbsp; Continue</ion-button>
+          <br />
+          <ion-button expand="full"
+            ><ion-icon :icon="sendOutline"></ion-icon>&nbsp;
+            Continue</ion-button
+          >
         </ion-card-content>
       </ion-card>
     </ion-content>
   </ion-page>
 </template>
 <script setup lang="ts">
-import { IonPage, IonContent, IonCard, IonCardHeader, IonCardContent, IonCardTitle, IonCardSubtitle, IonDatetimeButton, IonModal, IonDatetime, IonSegment, IonSegmentButton, IonLabel, IonButtons, IonButton, IonList, IonItem, IonSelect, IonSelectOption } from '@ionic/vue';
-import { useRoute } from 'vue-router';
-import { onMounted, ref, watch } from 'vue';
-import { getOutletById, getOutletFloor, getOutletTableFloor, storeRsvp } from '@/composables/Http';
-import { getOrdinalSuffix, imageUrl, currencyIDR } from '@/composables/Utils';
-import { getStore } from '@/composables/storage';
-import { sendOutline } from 'ionicons/icons';
+import {
+  IonPage,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardContent,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonDatetimeButton,
+  IonModal,
+  IonDatetime,
+  IonSegment,
+  IonSegmentButton,
+  IonLabel,
+  IonButtons,
+  IonButton,
+  IonList,
+  IonItem,
+  IonSelect,
+  IonSelectOption,
+} from "@ionic/vue";
+import { useRoute } from "vue-router";
+import { computed, onMounted, ref, watch } from "vue";
+import {
+  getOutletById,
+  getOutletFloor,
+  getOutletTableFloor,
+  storeRsvp,
+} from "@/composables/Http";
+import { getOrdinalSuffix, imageUrl, currencyIDR } from "@/composables/Utils";
+import { getStore } from "@/composables/storage";
+import { sendOutline } from "ionicons/icons";
 
 const route = useRoute();
 const outlet_id: any = ref(route.params.id);
-const outlet_details:any = ref([]);
-const floorSelected: any = ref('1');
+const outlet_details: any = ref([]);
+const floorSelected: any = ref("1");
 const tableSelected: any = ref([]);
 const floors: any = ref([]);
 const tables: any = ref([]);
 const startDate: any = ref(route.query.startDate);
 const endDate: any = ref(route.query.endDate);
-const dateSelected: any = ref('');
-const payment_method = ref('transfer');
+const dateSelected = ref(new Date().toJSON());
+// const dateSelected: any = computed({
+//   get() {
+//     return new Date().toJSON().split("T")[0];
+//   },
+//   // set(newVal) {
+//   //   console.log(newVal);
+//   // },
+// });
+const payment_method = ref("transfer");
 const carts: any = ref([]);
-watch(floorSelected, async () => await getOutletTableFloor(outlet_id.value, floorSelected.value, startDate.value));
+watch(
+  floorSelected,
+  async () =>
+    await getOutletTableFloor(
+      outlet_id.value,
+      floorSelected.value,
+      startDate.value
+    )
+);
 
 const outletDetail = async () => {
-  let resp: any = await getOutletById(outlet_id.value);
+  const resp: any = await getOutletById(outlet_id.value);
   if (resp.data.code === 200) outlet_details.value = resp.data.data;
-}
+};
 
 const selectTable = (data: any, table: any, floor: any) => {
-  let item: any = carts.value.find((item:any) => item.id == table);
+  const item: any = carts.value.find((item: any) => item.id == table);
   if (!item) {
     carts.value.push({
       id: data.id,
@@ -121,36 +177,51 @@ const selectTable = (data: any, table: any, floor: any) => {
     });
   } else {
     carts.value = carts.value.filter((item: any) => item.id != table);
-
   }
 
   console.log(carts.value);
 };
 
-
 const prepareForRsvp = async () => {
-
-  let user_id = getStore("_id");
-  let data = JSON.stringify(carts.value);
-  let resp: any = await storeRsvp(data, outlet_id.value, user_id, dateSelected.value, payment_method.value)
+  const user_id = getStore("_id");
+  const data = JSON.stringify(carts.value);
+  const resp: any = await storeRsvp(
+    data,
+    outlet_id.value,
+    user_id,
+    dateSelected.value,
+    payment_method.value
+  );
   console.log(resp);
-
 };
 const getFloor = async () => {
-  let resp: any = await getOutletFloor(outlet_id.value, startDate.value);
+  const resp: any = await getOutletFloor(outlet_id.value, startDate.value);
   if (resp.data.code === 200) {
     floors.value = resp.data.data;
   }
 };
 const getTables = async () => {
-  let resp: any = await getOutletTableFloor(outlet_id.value, floorSelected.value, startDate.value);
+  const resp: any = await getOutletTableFloor(
+    outlet_id.value,
+    floorSelected.value,
+    startDate.value
+  );
   if (resp.data.code === 200) {
     tables.value = resp.data.data;
   }
-}
+};
 onMounted(async () => {
   await outletDetail();
   await getFloor();
   await getTables();
 });
 </script>
+
+<style lang="scss" scoped>
+ion-datetime-button {
+  &.ios,
+  &.md {
+    justify-content: start;
+  }
+}
+</style>
